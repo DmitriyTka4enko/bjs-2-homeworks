f39f54f6d8a3c11aa32650410d8ac1f73db859e9
Dmitriy Tkachenko <Tkachenko_D_V@mail.ru>
2023-10-10 15:23:59 +0900

    Внесены правки с учетом замечаний от преподавателя

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,13 +1,12 @@
 "use strict"
+
 function solveEquation(a, b, c) {
-  let arr = [];
+	let arr = [];
 	let x1;
 	let x2;
-  const d = b ** 2 - 4 * a * c;
+	const d = b ** 2 - 4 * a * c;
 
-	if (d < 0) {
-		arr = [];
-	} else if (d === 0) {
+	if (d === 0) {
 		x1 = -b / (2 * a);
 		arr.push(x1);
 	} else if (d > 0) {
@@ -16,14 +15,13 @@ function solveEquation(a, b, c) {
 		arr.push(x1, x2);
 	}
 
-  return arr;
+	return arr;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  let monthlyRate = percent / 100 / 12;
+	let monthlyRate = percent / 100 / 12;
 	let mainDebt = amount - contribution;
 	let monthlyPayment = mainDebt * (monthlyRate + (monthlyRate / (((1 + monthlyRate) ** countMonths) - 1)));
-	let totalDebtSum = parseFloat((monthlyPayment * countMonths).toFixed(2));
 
-	return totalDebtSum;
+	return parseFloat((monthlyPayment * countMonths).toFixed(2));
 }
