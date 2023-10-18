ba6ca4a7206847a205326fa20f7dd7338656c4c0
Dmitriy Tkachenko <Tkachenko_D_V@mail.ru>
2023-10-18 14:03:58 +0900

    Task 4 done

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,39 @@
 function Student(name, gender, age) {
-  
+	this.name = name;
+	this.gender = gender;
+	this.age = age;
+	this.marks = []
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  
+	this.subject = subjectName;
 }
 
 Student.prototype.addMarks = function (...marks) {
-  
+	if (!this.hasOwnProperty('marks')) {
+		return 0;
+	}
+	this.marks.push(...marks)
 }
 
 Student.prototype.getAverage = function () {
-  
+	if (!this.hasOwnProperty('marks') || this.marks.length === 0) {
+		return 0;
+	}
+	return parseFloat((this.marks.reduce((acc, i) => acc + i) / this.marks.length).toFixed(1))
 }
 
 Student.prototype.exclude = function (reason) {
-  
+	delete this.subject;
+	delete this.marks;
+	this.excluded = reason
 }
+
+let student1 = new Student('Василиса', 'женский', '19');
+student1.setSubject('Algebra');
+student1.addMarks(4, 5, 4, 5);
+
+
+let student2 = new Student('Артём', 'мужской', '25');
+student2.setSubject('Geometry');
+student2.exclude('прогулы')
